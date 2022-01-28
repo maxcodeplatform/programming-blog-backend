9f7af3a01b23dcb749509c7cb95de81e34bb3c20
milon19 <milonmahato67@gmail.com>
2022-01-28 13:46:13 +0600

    PB-3: change folder structure

--- programming_blog/settings.py
+++ programming_blog/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-from config.env_config_loader import Config
+from core.env_config_loader import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
