8149a03e072bc8bb903d85f9e9ab60fd934accda
milon19 <milonmahato67@gmail.com>
2022-01-30 02:59:25 +0600

    PB-4: add support for postgres database

--- programming_blog/settings.py
+++ programming_blog/settings.py
@@ -60,13 +60,7 @@ WSGI_APPLICATION = 'programming_blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# TODO: get from .env and setup postgres
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = config.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
